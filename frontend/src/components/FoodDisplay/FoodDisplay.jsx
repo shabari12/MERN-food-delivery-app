04b82659cb8d4f8f6261fca9a437c1673a3d5c5a
Shabari Vignesh <113418289+shabari12@users.noreply.github.com>
2024-06-26 23:52:29 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
+
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
 
@@ -9,19 +10,20 @@ const FoodDisplay = ({ category }) => {
     <div className="food-display" id="food-display">
       <h2>Top Dishes Near You</h2>
       <div className="food-display-list">
-        {food_list.map((items, index) => {
-          if ((category === "All" || category === items.category)) {
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
             return (
               <FoodItem
-                key={index}
-                id={items._id}
-                name={items.name}
-                description={items.description}
-                price={items.price}
-                image={items.image}
+                key={item._id}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
               />
             );
           }
+          return null; // Ensure to return a value in all cases
         })}
       </div>
     </div>
